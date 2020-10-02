f5aed5077f1bb1e7ededcf7b64c9aa2414ca9c92
andryushque <andrew.ustinov23@gmail.com>
2020-10-02 15:30:25 +0300

    Create: header top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-  // js code
+  /*=== Header ===*/
+  const buttonEmail = $(".nav__link--email");
+  const buttonCopy = $(".nav__link-button");
+  buttonEmail.on("click", function (event) {
+    event.preventDefault();
+    buttonEmail.toggleClass("active");
+    buttonCopy.toggleClass("active");
+  });
+
+  const buttonPhone = $(".nav__link--phone");
+  const buttonClose = $(".submenu__close");
+  const submenu = $(".nav__submenu");
+  const overlay = $(".nav-wrapper__overlay");
+  buttonPhone.on("mouseover", function (event) {
+    event.preventDefault();
+    submenu.addClass("active");
+    buttonPhone.addClass("active");
+    overlay.addClass("active");
+  });
+  buttonClose.on("click", function (event) {
+    event.preventDefault();
+    submenu.removeClass("active");
+    buttonPhone.removeClass("active");
+    overlay.removeClass("active");
+  });
 });
